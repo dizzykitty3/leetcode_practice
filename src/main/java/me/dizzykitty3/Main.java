cb63f92caa78ac2db5e61bbc713dd2197d44c6ae
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2023-12-19 22:35:39 +0800

    code style

--- src/main/java/me/dizzykitty3/Main.java
+++ src/main/java/me/dizzykitty3/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // test outputs here
+        // Test outputs here
         Solution solution = new Solution();
 
         // 1. Two Sum
--- src/main/java/me/dizzykitty3/Solution.java
+++ src/main/java/me/dizzykitty3/Solution.java
@@ -1,8 +1,8 @@
 package me.dizzykitty3;
 
 public class Solution {
-    /*
-     * 1. Two Sum [easy]
+    /**
+     * 1. Two Sum [Easy] #Array #HashTable
      * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
      * You may assume that each input would have exactly one solution, and you may not use the same element twice.
      * You can return the answer in any order.
@@ -10,11 +10,11 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
         final int length = nums.length;
         for (int i = 0; i < length; i++) {
-            int gap = target - nums[i]; // avoid doing extensive addition operations
+            int gap = target - nums[i]; // Avoid doing extensive addition operations
             for (int j = i + 1; j < length; j++) { // j starts with i + 1
-                if (nums[j] == gap) return new int[]{i, j}; // return the answer
+                if (nums[j] == gap) return new int[]{i, j}; // Return the answer
             }
         }
-        return new int[]{-1, -1}; // return {-1, -1} if there is no answer
+        return new int[]{-1, -1}; // Return [-1, -1] if there is no answer
     }
 }
